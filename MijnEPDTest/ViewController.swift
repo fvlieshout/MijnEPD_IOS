ee40c4c7a951337f1ebe4dc79fc639b073139b28
Denise van Diermen <denisevandiermen@macbookandenise.lan>
2018-07-27 10:21:09 +0200

    Initial Commit

---
+++ MijnEPDTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MijnEPDTest
+//
+//  Created by Denise van Diermen on 27-07-18.
+//  Copyright © 2018 Floor van Lieshout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
