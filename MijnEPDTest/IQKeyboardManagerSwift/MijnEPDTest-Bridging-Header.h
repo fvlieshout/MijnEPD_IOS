8014b7423ce01abd20177105d422579b8fefb9e4
Miró Scholten <Miro_Scholten@macbook-pro-van-miro.local>
2018-08-14 18:15:00 +0200

    Plus knop nieuw document Nieuwe documenten scherm incl toevoegen afbeelding uit camera of bibliotheek

---
+++ MijnEPDTest/IQKeyboardManagerSwift/MijnEPDTest-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MijnEPDTest-Bridging-Header.h
+//  MijnEPDTest
+//
+//  Created by Miró Scholten on 14-08-18.
+//  Copyright © 2018 Floor van Lieshout. All rights reserved.
+//
+
+#ifndef MijnEPDTest_Bridging_Header_h
+#define MijnEPDTest_Bridging_Header_h
+
+
+#endif /* MijnEPDTest_Bridging_Header_h */
---
+++ MijnEPDTest/NewDocumentViewController.swift
@@ -0,0 +1,86 @@
+//
+//  NewDocumentViewController.swift
+//  MijnEPDTest
+//
+//  Created by Miró Scholten on 14-08-18.
+//  Copyright © 2018 Floor van Lieshout. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewDocumentViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageViewer: UIImageView!
+    @IBOutlet weak var dateField: UITextField!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var ArtsField: UITextField!
+    
+    @IBOutlet weak var descField: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dateField.delegate = self
+        locationField.delegate = self
+        ArtsField.delegate = self
+    }
+    
+    @IBAction func openCamera(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo source", message: "Choose source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action: UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            } else {
+                    let alertController = UIAlertController(title: "mijnEPD", message:
+                        "Camera is niet beschikbaar", preferredStyle: UIAlertControllerStyle.alert)
+                    alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            
+           
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Foto Bibliotheek", style: .default, handler: { (action: UIAlertAction) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Annuleer", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+        
+    }
+    
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageViewer.image = image
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    
+}
+
+extension NewDocumentViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
