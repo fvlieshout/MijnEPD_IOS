b1fb355867ed031c5c4a3c333384d2fb926ec40c
Noel <ns.bainathsah@gmail.com>
2018-08-27 13:51:52 +0200

    pickerview, opslaanknop deels functioneel

--- MijnEPDTest/NewDocumentViewController.swift
+++ MijnEPDTest/NewDocumentViewController.swift
@@ -7,21 +7,27 @@
 //
 
 import Foundation
+import SQLite3
 import UIKit
 
-class NewDocumentViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
+class NewDocumentViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    var db: OpaquePointer?
     //outlets voor de tekstvelden en radiobutton
     
     @IBOutlet weak var imageViewer: UIImageView!
+    @IBOutlet weak var descField: UITextView!
     @IBOutlet weak var dateField: UITextField!
     @IBOutlet weak var locationField: UITextField!
     @IBOutlet weak var ArtsField: UITextField!
     
-    @IBOutlet weak var descField: UITextView!
-    
     @IBOutlet weak var labUitslag: DLRadioButton!
     
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let specialismen = ["Anesthesiologie", "Cardiologie", "Dermatologie", "Gynaecologie", "Huisartsgeneeskunde", "Interne geneeskunde", "Keel-neus-oorheelkunde",
+                        "Kindergeneeskunde", "Klinische genetica", "Longgeneeskunde", "Maag-darm-leverziekten", "Neurologie", "Oogheelkunde", "Psychiatrie"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,9 +42,108 @@ class NewDocumentViewController: UIViewController, UIImagePickerControllerDelega
         descField.textColor = UIColor.lightGray
         
         labUitslag.isMultipleSelectionEnabled = false
-        
+        DatabaseConnector.init()
         
 }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return specialismen[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return specialismen.count
+    }
+    
+    @IBAction func opslaanInfo(_ sender: UIButton) {
+        //getting values from textfields
+        let beschrijving = descField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let datum = dateField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let locatie = locationField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let artsNaam = ArtsField.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        //validating that values are not empty
+        if(beschrijving?.isEmpty)!{
+            descField.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        
+        if(datum?.isEmpty)!{
+            dateField.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        
+        if(locatie?.isEmpty)!{
+            locationField.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        
+        if(artsNaam?.isEmpty)!{
+            ArtsField.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        
+        
+        
+    
+        //creating a statement
+        var stmt: OpaquePointer?
+        
+        //the insert query
+        let queryString = "INSERT INTO mijnEPD (beschrijving, datum, locatie, artsNaam) VALUES (?,?,?,?)"
+        
+        //preparing the query
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing insert: \(errmsg)")
+            return
+        }
+        
+        //binding the parameters
+        if sqlite3_bind_text(stmt, 1, beschrijving, -1, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding beschrijving: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_bind_int(stmt, 2, (datum! as NSString).intValue) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding datum: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_bind_text(stmt, 1, locatie, -1, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding locatie: \(errmsg)")
+            return
+        }
+        
+        if sqlite3_bind_text(stmt, 1, artsNaam, -1, nil) != SQLITE_OK{
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure binding Artsnaam: \(errmsg)")
+            return
+        }
+        
+        //executing the query to insert values
+        if sqlite3_step(stmt) != SQLITE_DONE {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("failure inserting mijnEPDdocumnent: \(errmsg)")
+            return
+        }
+        
+        //emptying the textfields
+        descField.text=""
+        dateField.text=""
+        locationField.text=""
+        ArtsField.text=""
+        
+        
+        //displaying a success message
+        print("mijnEPDdocument is succesvol opgeslagen")
+        
+    }
     
     @IBAction func radioAction(_ sender: DLRadioButton) {
         if sender.tag == 1 {
