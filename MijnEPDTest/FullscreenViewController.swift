0cbbb7d2e04bd9fb0bca5ac47a15a0d8dcbbd260
Floor van Lieshout <floorvanlieshout@hotmail.com>
2019-04-28 18:11:18 +0200

    Extra class toegevoegd die de foto fullscreen zou moeten kunnen tonen en daarin dan ook kan zoomen. Dit moet nog in de rest van de code ingeplakt worden

---
+++ MijnEPDTest/FullscreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FullscreenViewController.swift
+//  MijnEPDTest
+//
+//  Created by Thijs Frederik van Haaps on 28/04/2019.
+//  Copyright © 2019 Floor van Lieshout. All rights reserved.
+//
+
+import UIKit
+
+class FullscreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //let imageView = ImageZoomView(frame: <#T##CGRect#>, image: <#T##UIImage?#>)
+    }
+
+}
